913fb4e06db34c4f6d16d257fee654268226384b
HigeMan <kohei.otani@tutanota.com>
2022-11-05 19:01:06 +0900

    - update: kojin data model

---
+++ lib/models/kojin_data.dart
@@ -0,0 +1,24 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'kojin_data.freezed.dart';
+part 'kojin_data.g.dart';
+
+@freezed
+class KojinData with _$KojinData {
+
+  const factory KojinData(
+      {required int id,
+      required String firstName,
+      required String lastName,
+      required String alias,
+      required int bornDate,
+      required int deathDate,
+      required int age,
+      required String country,
+      required List<String> jobs,
+      required String profile}
+      ) = _KojinData;
+  factory KojinData.fromJson(Map<String, dynamic> json) => _$KojinDataFromJson(json);
+
+  
+}
