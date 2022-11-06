afbcdab3f9d5b95d0e1c84d54223f20fe51f26ca
HigeMan <kohei.otani@tutanota.com>
2022-11-06 20:03:41 +0900

    - update: added dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_api_test/providers.dart';
 
 import 'models/kojin_data.dart';
 import 'pages/home_page.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const ProviderScope(child: MyApp()));
 }
 
