a882fdeb3e22c055a7987bc572f8e8cf23302332
HigeMan <kohei.otani@tutanota.com>
2022-11-06 19:53:32 +0900

    - update: added kojin data freezed class

--- lib/models/kojin_data.dart
+++ lib/models/kojin_data.dart
@@ -7,16 +7,17 @@ part 'kojin_data.g.dart';
 class KojinData with _$KojinData {
 
   const factory KojinData(
-      {required int id,
-      required String firstName,
-      required String lastName,
-      required String alias,
-      required int bornDate,
-      required int deathDate,
-      required int age,
-      required String country,
-      required List<String> jobs,
-      required String profile}
+      {
+        // required int? id,
+      required String? firstName,
+      required String? lastName,
+      required String? alias,
+      // required int? bornDate,
+      // required int? deathDate,
+      // required int? age,
+      required String? country,
+      required List<String>? jobs,
+      required String? profile}
       ) = _KojinData;
   factory KojinData.fromJson(Map<String, dynamic> json) => _$KojinDataFromJson(json);
 
