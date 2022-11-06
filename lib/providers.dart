ddaf7ef10609a56883a2c22660016eab981e7bfe
HigeMan <kohei.otani@tutanota.com>
2022-11-06 19:53:49 +0900

    - update: providers

--- lib/providers.dart
+++ lib/providers.dart
@@ -2,10 +2,27 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'api/gsheets_client.dart';
 import 'models/kojin_data.dart';
+import 'models/test.dart';
+import 'repository/repository.dart';
+import 'repository/test_repository.dart';
 
-final deadPeopleListFutureProvider = 
-FutureProvider<List<KojinData>>((ref) async {
-  final deadRepo = GSheetsClient();
-  final deadPeopleList = await deadRepo.fetchData();
-  return deadPeopleList;
+// final listProvider = 
+// FutureProvider<List<KojinData>>((ref) async {
+//   final deadRepo = GSheetsClient();
+//   final deadPeopleList = await deadRepo.fetchData();
+//   return deadPeopleList ?? [];
+// });
+
+final repositoryProvider = Provider((ref) => Repository());
+
+final listProvider = FutureProvider<List<KojinData>>((ref) async {
+  final repo = ref.watch(repositoryProvider);
+  return await repo.fetchList();
+});
+
+final repositoryTestProvider = Provider((ref) => TestRepository());
+
+final testProvider = FutureProvider<List<Test>>((ref) async {
+  final testRepo = ref.watch(repositoryTestProvider);
+  return await testRepo.fetchTestList();
 });
